dee7b8807408ea417a80a6c62b536f3e716c7f91
Rosana Rodriguez Berrios <157861390+RosanaRodz@users.noreply.github.com>
2024-11-13 19:41:55 -0400

    Message popup preset messages by filter updated

--- resources/views/backDashboard.php
+++ resources/views/backDashboard.php
@@ -66,7 +66,7 @@ require __DIR__ . '/partials/header.php';
                 <div class="stat-card">
                     <div class="stat-header">
                         <h2 class="stat-title">Solicitantes</h2>
-                        <a href="#" class="view-all">ver todos</a>
+                        <a href="/admin/requests" class="view-all">ver todos</a>
                     </div>
                     <div class="stat-number">75</div>
                        <img src="https://img.icons8.com/?size=100&id=tfnuCxzS4iEn&format=png&color=1A1A1A" alter="Applicants Icon" class="main-icons">
@@ -166,7 +166,6 @@ require __DIR__ . '/partials/header.php';
                      
                     <div class="message-popup" id="messagePopup" style="display: none">
                         <!-- Close button in the top-right corner -->
-                        <!-- <img src="https://img.icons8.com/?size=100&id=71200&format=png&color=1A1A1A" alt="Close" class="close-icon" id="closePopup"> -->
                         <a href="#" class="plain-action" id="closePopup"><i class="las la-times"></i></a>
                         
                         <!-- Popup title -->
