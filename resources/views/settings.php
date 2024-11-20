1bbbf1d3b6d9772b538cf1f1497e389a8df8dba0
ricmozz8 <cupcakethief2311@gmail.com>
2024-11-20 14:40:39 -0400

    fix messages modal

--- resources/views/settings.php
+++ resources/views/settings.php
@@ -234,7 +234,7 @@ require __DIR__ . '/partials/header.php';
         <h2 class="message-title">Editar mensaje predeterminado para aceptados</h2>
 
         <!-- Form area -->
-        <form method="post" action="settings/e/approved">
+        <form method="post" action="settings/e/approved" style="width: 100%;">
             <!-- Message input area -->
             <textarea name="content" class="message-textarea" placeholder="Escriba su mensaje aquí..." aria-label="Message input"><?= $messages['approved']['content'] ?></textarea>
             <input name="id" type="hidden" value="<?= $messages['approved']['id'] ?>">
@@ -255,7 +255,7 @@ require __DIR__ . '/partials/header.php';
         <h2 class="message-title">Editar mensaje predeterminado para denegados</h2>
 
         <!-- Form area -->
-        <form method="post" action="settings/e/rejected">
+        <form method="post" action="settings/e/rejected" style="width: 100%;">
             <!-- Message input area -->
             <textarea name="content" class="message-textarea" placeholder="Escriba su mensaje aquí..." aria-label="Message input"><?= $messages['denied']['content'] ?></textarea>
             <input name="id" type="hidden" value="<?= $messages['denied']['id'] ?>">
@@ -275,7 +275,7 @@ require __DIR__ . '/partials/header.php';
         <h2 class="message-title">Editar mensaje predeterminado para todos</h2>
 
         <!-- Form area -->
-        <form method="post" action="settings/e/all">
+        <form method="post" action="settings/e/all" style="width: 100%;">
             <!-- Message input area -->
             <textarea name="content" class="message-textarea" placeholder="Escriba su mensaje aquí..." aria-label="Message input"><?= $messages['all']['content'] ?></textarea>
             <input name="id" type="hidden" value="<?= $messages['all']['id'] ?>">
