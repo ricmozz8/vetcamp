68184b6c7cc4b7680926ddbd794b405279c53e2e
ricmozz8 <cupcakethief2311@gmail.com>
2024-11-27 15:32:06 -0400

    fix on 404 image

--- resources/views/error.php
+++ resources/views/error.php
@@ -9,7 +9,7 @@ require __DIR__ . '/partials/header.php';
 <body class="server-error-body">
     <div class="error-container">
         <div class="iso-centered">
-            <img src="<?= asset('logo/SVG/vetcamp_iso_w.svg') ?>" alt="">
+            <img src="/<?= asset('logo/SVG/vetcamp_iso_w.svg') ?>" alt="">
             <h2>Ha ocurrido un error!</h2>
             <?php if ($code == 404) { ?>
                 <p>La página solicitada no existe.</p>
