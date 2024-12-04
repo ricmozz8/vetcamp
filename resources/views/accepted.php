0c913273b78c664afa8c67625465384b9b07cad9
ricmozz8 <cupcakethief2311@gmail.com>
2024-12-04 17:15:09 -0400

    added structure of accepted view

--- resources/views/accepted.php
+++ resources/views/accepted.php
@@ -26,13 +26,13 @@ require_once __DIR__ . '/partials/header.php';
             <div class="accepted-grouped">
                 <div class="accepted-card">
                     <div class="accepted-card-header">
-                        <div class="flex-min">
-                            
-                        <h2 class="accepted-card-title">Sección 1 (15 al 20 de julio)</h2>
+                        <div class="">
+
+                            <h2 class="accepted-card-title">Sección 1 (15 al 20 de julio)</h2>
                             <p>
-                            <i class="las la-users"></i>
-                            1/14 estudiantes
-                        </p>
+                                <i class="las la-users"></i>
+                                1/14 estudiantes
+                            </p>
                         </div>
                         <button class="main-action-bright">
                             <i class="las la-envelope"></i>
@@ -44,7 +44,157 @@ require_once __DIR__ . '/partials/header.php';
                             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
                             <h3>Chill Guy</h3>
                         </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
                     </div>
+
+                </div>
+                <div class="accepted-card">
+                    <div class="accepted-card-header">
+                        <div class="">
+
+                            <h2 class="accepted-card-title">Sección 1 (15 al 20 de julio)</h2>
+                            <p>
+                                <i class="las la-users"></i>
+                                1/14 estudiantes
+                            </p>
+                        </div>
+                        <button class="main-action-bright">
+                            <i class="las la-envelope"></i>
+                            Enviar correo
+                        </button>
+                    </div>
+                    <div class="accepted-card-list">
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="accepted-card">
+                    <div class="accepted-card-header">
+                        <div class="">
+
+                            <h2 class="accepted-card-title">Sección 1 (15 al 20 de julio)</h2>
+                            <p>
+                                <i class="las la-users"></i>
+                                1/14 estudiantes
+                            </p>
+                        </div>
+                        <button class="main-action-bright">
+                            <i class="las la-envelope"></i>
+                            Enviar correo
+                        </button>
+                    </div>
+                    <div class="accepted-card-list">
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="accepted-card">
+                    <div class="accepted-card-header">
+                        <div class="">
+
+                            <h2 class="accepted-card-title">Sección 1 (15 al 20 de julio)</h2>
+                            <p>
+                                <i class="las la-users"></i>
+                                1/14 estudiantes
+                            </p>
+                        </div>
+                        <button class="main-action-bright">
+                            <i class="las la-envelope"></i>
+                            Enviar correo
+                        </button>
+                    </div>
+                    <div class="accepted-card-list">
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                    </div>
+
                 </div>
 
 
@@ -52,6 +202,8 @@ require_once __DIR__ . '/partials/header.php';
 
         </main>
     </div>
+
+    <?php require_once('partials/footer.php'); ?>
 </body>
 
 </html>
