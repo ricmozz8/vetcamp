ecb657a8205f8fea5a7be8504fcea9505ebef816
ricmozz8 <cupcakethief2311@gmail.com>
2024-12-04 16:54:00 -0400

    added components to accepted view

--- resources/views/accepted.php
+++ resources/views/accepted.php
@@ -23,7 +23,31 @@ require_once __DIR__ . '/partials/header.php';
                 <h1 class="welcome"> Aceptados </h1>
             </header>
 
-            
+            <div class="accepted-grouped">
+                <div class="accepted-card">
+                    <div class="accepted-card-header">
+                        <div class="flex-min">
+                            
+                        <h2 class="accepted-card-title">Sección 1 (15 al 20 de julio)</h2>
+                            <p>
+                            <i class="las la-users"></i>
+                            1/14 estudiantes
+                        </p>
+                        </div>
+                        <button class="main-action-bright">
+                            <i class="las la-envelope"></i>
+                            Enviar correo
+                        </button>
+                    </div>
+                    <div class="accepted-card-list">
+                        <div class="accepted-user-card">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F002%2F901%2F902%2F95c.png&f=1&nofb=1&ipt=e8f6df058776a57d715f8504ac35876a754aa07ad0211a2b2cc5263753f7929f&ipo=images" alt="userIMG">
+                            <h3>Chill Guy</h3>
+                        </div>
+                    </div>
+                </div>
+
+
 
 
         </main>
