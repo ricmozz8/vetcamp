6090bf3478d5ae3d2ddad39d7ea54b60259cd259
Joseph Figueroa <joseph.figueroa4@upr.edu>
2024-12-12 16:00:51 -0400

    filter Name and email RegisteredController

--- app/controllers/RegisteredController.php
+++ app/controllers/RegisteredController.php
@@ -36,6 +36,27 @@ class RegisteredController extends Controller
         $offset = ($page - 1) * $perPage;
         $users = array_slice($allUsers, $offset, $perPage);
 
+        // Filtro 
+        // storing users
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $palabra = $_POST['search'];
+                if (!empty($palabra)) {
+                    $searchTerm = $palabra . "%";
+                    // Realizar la búsqueda en las columnas relevantes
+                    $users = User::findLike([
+                        'first_name' => $searchTerm,
+                        'last_name' => $searchTerm,
+                        'email' => $searchTerm
+                    ]);
+                } else {
+                    $users = User::allof('user');
+                }
+        } else {
+            $users = User::allof('user');
+        }
+        //render_view('registered', ["users" => $users, 'selected' => 'registered'], 'Registered');
+        // end filtro
+
         // your index view here
         render_view('registered', [
             "users" => $users,
