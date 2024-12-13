e0ec78b1a4026e055d28247153144e3632d3ce3a
ricmozz8 <cupcakethief2311@gmail.com>
2024-12-13 01:05:57 -0400

    security CSP policy to avoid blocked files

--- resources/views/partials/header.php
+++ resources/views/partials/header.php
@@ -1,6 +1,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; object-src 'self' blob:; style-src 'self'; frame-src 'self'">
+
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="/<?= asset("logo/SVG/vetcamp-icon-yellow.svg") ?>" type="image/x-icon">
