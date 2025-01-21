d2afd18ec4aa5a3dee91de2519bbee95178a5189
ricmozz8 <cupcakethief2311@gmail.com>
2025-01-21 10:30:21 -0400

    added beta badge to application logo

--- resources/views/partials/applicationLogo.php
+++ resources/views/partials/applicationLogo.php
@@ -1,1 +1,4 @@
+<div style="display: flex; align-items: center; width: fit-content; margin: auto; gap: 10px">
 <img draggable="false" class="app-logotype" src="/<?= asset('logo/SVG/vetcamp-full-black.svg') ?>" alt="Vetcamp Logo">
+<p style="padding: 5px; background-color: #000; color: #fff; border-radius: 5px; font-weight: bold;">BETA</p>
+</div>
