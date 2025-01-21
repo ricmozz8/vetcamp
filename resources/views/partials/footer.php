27b12763a804e3eba321bc4797564c3a8e3beea1
ricmozz8 <cupcakethief2311@gmail.com>
2025-01-21 10:33:01 -0400

    Added Vetcamp version info to footer

--- resources/views/partials/footer.php
+++ resources/views/partials/footer.php
@@ -1,4 +1,7 @@
 <footer>
-        <p>&copy; <?php echo date('Y'); ?> | Universidad de Puerto Rico Arecibo</p>
+        <div>
+                <p>Vetcamp | v0.1.408 (BETA)</p>
+                <p>&copy; <?php echo date('Y'); ?> | Universidad de Puerto Rico Arecibo</p>
+        </div>
         <img class="university-logo" src="https://upra.edu/wp-content/uploads/2015/08/arecibo.png" alt="logo upra">
 </footer>
