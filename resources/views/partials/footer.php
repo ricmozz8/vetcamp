85c7bb11c9747ca951cbbe1887d61ae80b23d1be
ricmozz8 <cupcakethief2311@gmail.com>
2025-01-27 02:14:22 -0800

    added credits link to footer

--- resources/views/partials/footer.php
+++ resources/views/partials/footer.php
@@ -2,6 +2,7 @@
         <div>
                 <p>Vetcamp | v0.1.408 (BETA)</p>
                 <p>&copy; <?php echo date('Y'); ?> | Universidad de Puerto Rico Arecibo</p>
+                <a href="/credits" class="no-deco-action">Créditos</a>
         </div>
         <img class="university-logo" src="https://upra.edu/wp-content/uploads/2015/08/arecibo.png" alt="logo upra">
 </footer>
