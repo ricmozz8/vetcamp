57ea799643c0812d20d9c8adb44c2aefe7d10db9
ricmozz8 <cupcakethief2311@gmail.com>
2025-02-01 16:57:56 -0400

    Updated label text for extra_info field

--- resources/views/modals/confirmSubmitApplicationModal.php
+++ resources/views/modals/confirmSubmitApplicationModal.php
@@ -5,7 +5,7 @@
         <h2>¿Estás seguro de que quieres enviar la solicitud?</h2>
         <p>Tu solicitud será evaluada con la información proporcionada.</p>
         <textarea name="extra_info" id="extra_info" placeholder="Introduce alguna información adicional que quieras compartir, por ejemplo si sufres alguna condición medica o alergias que los coordinadores del campamento deban saber..."><?= $application->extra_notes ?></textarea>
-        <label class="mini-label" for="extra_info">Esta información es opcional</label>
+        <label class="mini-label" for="extra_info">Esta información es opcional, puedes dejarlo en blanco.</label>
         <div class="modal-actions">
             <a href="#" class="main-action-bright" onclick="closeModal('confirmSubmitApplicationModal')">Cancelar</a>
             <button class="main-action-bright gradiented">
