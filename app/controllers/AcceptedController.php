e7a460f4456d8c61d1cc812c58680e51fa0f77fe
Joseph Figueroa <joseph.figueroa4@upr.edu>
2025-02-12 13:17:20 -0400

    Fix AcceptedController

--- app/controllers/AcceptedController.php
+++ app/controllers/AcceptedController.php
@@ -20,31 +20,16 @@ class AcceptedController extends Controller
         
         $approvedApplicants = User::approvedApplicants();
 
-        // Agrupar los usuarios en tres listas según la sesión preferida
-        $sessions = [
-            'Sesion 1' => [],
-            'Sesion 2' => [],
-            'Sesion 3' => []
-        ];
-
         foreach ($approvedApplicants as $user) {
             $sessionId = $user['id_preferred_session'];
-            $sessionKey = 'Sin asignar'; // Valor por defecto en caso de error
 
-            if ($sessionId == 1) {
-                $sessionKey = 'Sesion 1';
-            } elseif ($sessionId == 2) {
-                $sessionKey = 'Sesion 2';
-            } elseif ($sessionId == 3) {
-                $sessionKey = 'Sesion 3';
-            }
 
 
             $pictureObj = Application::find($user['id_application'])->getProfilePicture();
             $src = "data:" . $pictureObj['type'] . ";base64," . base64_encode($pictureObj['contents']);
 
             // Agregar la información formateada
-            $sessions[$sessionKey][] = [
+            $sessions['Sesion '. $sessionId][] = [
                 'user_id'   => $user['user_id'],
                 'full_name' => User::find($user['user_id'])->first_name . ' ' . User::find($user['user_id'])->last_name,
                 'profile_picture' => $src,
