58bf82f3d35244fa8dd8b8ea344d93924f301e69
ricmozz8 <cupcakethief2311@gmail.com>
2025-02-15 07:34:46 -0400

    Handle missing limit dates in HomeController index method
    Added validation to check for the existence of limit dates in the database. If not found, a default limit date is created with today's date to ensure proper functionality. Adjusted view rendering to use validated or created limit dates.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -7,15 +7,26 @@ class HomeController extends Controller
 {
 
     /**
-     * This renders the index view.
+     * This renders the index view which shows the limit dates and the current sessions.
      *
-     *
+     * @note If this is the first time the app is setting up, the limit dates need to be changed on either
+     * the database or the admin panel
      * @return void
      */
     public static function index()
     {
+        // Validate that the limit dates exist
+        try {
+            $limitDates = LimitDate::find(1);
+        } catch (ModelNotFoundException $e) {
+            // if they don't exist, create a temporary one with today as the start and end date.
+            $limitDates = LimitDate::create(['id_date' => 1, 'start_date' => date('Y-m-d'), 'end_date' => date('Y-m-d')]);
+        }
+
+        $sessions = Session::all();
+
         // your index view here
-        render_view('home', ['sessions' => Session::all(), 'limit_dates' => LimitDate::find(1)], 'Solicita para el Vetcamp ' . date('Y'));
+        render_view('home', ['sessions' => $sessions, 'limit_dates' => $limitDates], 'Solicita para el Vetcamp ' . date('Y'));
     }
 
     // define your other methods here
@@ -25,7 +36,8 @@ class HomeController extends Controller
         render_view('branding', [], 'Marca');
     }
 
-    public static function credits(){
+    public static function credits()
+    {
         render_view('credits', [], 'Créditos');
     }
 }
