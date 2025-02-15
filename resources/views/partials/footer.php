adea148240cd5476b608604bd706fc2e4cd85774
ricmozz8 <cupcakethief2311@gmail.com>
2025-02-15 07:38:30 -0400

    formatted footer

--- resources/views/partials/footer.php
+++ resources/views/partials/footer.php
@@ -1,8 +1,8 @@
 <footer>
-        <div>
-                <p>Vetcamp | v0.2.0 (BETA)</p>
-                <p>&copy; <?php echo date('Y'); ?> | Universidad de Puerto Rico Arecibo</p>
-                <a href="/credits" class="no-deco-action">Créditos</a>
-        </div>
-        <img class="university-logo" src="https://upra.edu/wp-content/uploads/2015/08/arecibo.png" alt="logo upra">
+    <div>
+        <p>Vetcamp | v0.2.0 (BETA)</p>
+        <p>&copy; <?php echo date('Y'); ?> | Universidad de Puerto Rico Arecibo</p>
+        <a href="/credits" class="no-deco-action">Créditos</a>
+    </div>
+    <img class="university-logo" src="https://upra.edu/wp-content/uploads/2015/08/arecibo.png" alt="logo upra">
 </footer>
