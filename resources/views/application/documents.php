6d101cd4fc6ed9fb96ae71056834bb71a10d1dcd
ricmozz8 <cupcakethief2311@gmail.com>
2025-02-17 21:43:18 -0400

    Fixed confirmation buttons and texts

--- resources/views/application/documents.php
+++ resources/views/application/documents.php
@@ -244,7 +244,7 @@ require __DIR__ . '../../partials/header.php';
         </div>
         <div class="form-actions">
             <input type="hidden" name="stage" value="4">
-            <p>Se guardará la información una vez pulses 'siguiente'.</p>
+            <p>Se guardará la información una vez pulses siguiente.</p>
             <button type="submit" id="next-button" class="main-action-bright gradiented">Siguiente</button>
         </div>
     </form>
