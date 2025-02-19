0dca4518f793c38a44ee98061ddfbb707dc40e81
ricmozz8 <cupcakethief2311@gmail.com>
2025-02-19 06:42:20 -0400

    UI fixes for modals

--- resources/views/modals/createAdminModal.php
+++ resources/views/modals/createAdminModal.php
@@ -9,45 +9,45 @@
         <p>Este usuario podrá acceder al panel de administración</p>
         <br>
 
-        <form class="w-full" action="/admin/create" method="POST">
+        <form class="fas fa-" action="/admin/create" method="POST">
 
 
             <div class="form-group">
                 <label for="first_name">Nombre</label>
-                <input class="session-edit-input w-full" type="text" name="first_name" placeholder="Nombre" required>
+                <input class="" type="text" name="first_name" placeholder="Nombre" required>
             </div>
 
             <div class="form-group">
                 <label for="last_name">Apellido</label>
-                <input class="session-edit-input w-full" type="text" name="last_name" placeholder="Apellido" required>
+                <input class="" type="text" name="last_name" placeholder="Apellido" required>
             </div>
             <div class="form-group">
                 <label for="email">Correo</label>
-                <input class="session-edit-input w-full" type="email" name="email" placeholder="Correo" required>
+                <input class="" type="email" name="email" placeholder="Correo" required>
             </div>
 
             <div class="form-group">
-                <div class="flex">
+                <div class="flex-min">
                     <label for="password">Contraseña</label>
                     <span class="password-toggle" onclick="togglePassword()">
-                        Alternar mostrar <i class="fas fa-eye"></i>
+                         <i class="fas fa-eye"></i>
                     </span>
                 </div>
 
-                <input class="session-edit-input w-full" id="password" type="password" name="password" placeholder="Contraseña" required>
+                <input class="" id="password" type="password" name="password" placeholder="Contraseña" required>
             </div>
 
             <div class="form-group">
-                <div class="flex">
+                <div class="flex-min">
                     <label for="password">Confirmar Contraseña</label>
                     <span class="password-toggle password-toggle-c" onclick="togglePassword('password-confirmation', 'password-toggle-c')">
-                        Alternar mostrar <i class="fas fa-eye"></i>
+                        <i class="fas fa-eye"></i>
                     </span>
                 </div>
-                <input class="session-edit-input w-full" id="password-confirmation" type="password" name="password_confirmation" placeholder="Confirmar contraseña" required>
+                <input class="" id="password-confirmation" type="password" name="password_confirmation" placeholder="Confirmar contraseña" required>
             </div>
             <div class="modal-actions">
-                <div class="w-fit">
+                <div class="flex-min">
                     <input type="checkbox" name="notify">
                     <label for="notify">Notificar al correo</label>
                 </div>
