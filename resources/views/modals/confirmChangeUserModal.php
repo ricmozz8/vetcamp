41332ea34ecbe936215c39c1ec6f89d1338f22ac
Joseph Figueroa <joseph.figueroa4@upr.edu>
2025-03-06 15:47:07 -0400

    VET-3 Desactivación/Reactivación de cuentas -fix routes

--- resources/views/modals/confirmChangeUserModal.php
+++ resources/views/modals/confirmChangeUserModal.php
@@ -6,12 +6,12 @@
     <div class="modal-content">
         <span class="close-button" onclick="closeModal('confirmChangeUserModal')"><i class="fas fa-times"></i></span>
         <i class="fas fa-question-circle"></i>
-        <h2>¿Estás seguro de que quieres cambiar el estado?</h2>
-        <p>El estado del usuario cambiará en la base de datos.</p>
+        <h2>Esta acción cambiará el estado del usuario en el sistema. ¿Desea continuar?</h2>
+        <p>El estado del usuario se actualizará en el sistema.</p>
         <div class="modal-actions">
             <a href="#" class="main-action-bright" onclick="closeModal('confirmChangeUserModal')">Cancelar</a>
             <button type="button" class="main-action-bright gradiented" onclick="submitChangeStatus()">
-            <i class="fa-solid fa-pen"></i> Aceptar
+            <i class="fa-solid fa-gear"></i> Aceptar
             </button>
         </div>
     </div>
